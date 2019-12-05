1e5951171bc2bf4e08adff85c7fbb4d463ac8010
Thomas <tholmes59@gmail.com>
2019-12-05 11:24:45 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove()
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+document.body.appendChild(newHeader)
+newHeader.innerHTML = 'Tom is the champion'
+
